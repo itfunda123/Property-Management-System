01302aef8ac8769fc9993ca59d36233bc30b4927
unknown <melkywafula29@gmail.com>
2025-04-28 15:37:16 +0300

    footer styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,64 +10,67 @@ import Notifications from './components/Notifications';
 import Navbar1 from './components/Navbar1';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import Home from './pages/Home';         // <-- newly imported
-import AboutUs from './pages/AboutUs';   // <-- newly imported
-import ContactUs from './pages/ContactUs'; // <-- newly imported
+import Home from './pages/Home';
+import AboutUs from './pages/AboutUs';
+import ContactUs from './pages/ContactUs';
 
 function App() {
   return (
     <Router>
       <div className="d-flex flex-column min-vh-100">
-        <Routes>
+        {/* Main content that grows to push footer down */}
+        <div className="flex-grow-1">
+          <Routes>
 
-          {/* Public Routes with Navbar1 */}
-          <Route path="/login" element={
-            <>
-              <Navbar1 />
-              <Login />
-            </>
-          } />
-          <Route path="/register" element={
-            <>
-              <Navbar1 />
-              <Register />
-            </>
-          } />
+            {/* Public Routes with Navbar1 */}
+            <Route path="/login" element={
+              <>
+                <Navbar1 />
+                <Login />
+              </>
+            } />
+            <Route path="/register" element={
+              <>
+                <Navbar1 />
+                <Register />
+              </>
+            } />
 
-          {/* Tenant Routes */}
-          <Route path="/tenant-dashboard" element={
-            <PrivateRoute>
-              <Navbar />
-              <Dashboard />
-            </PrivateRoute>
-          } />
-          <Route path="/pay-rent" element={
-            <PrivateRoute>
-              <Navbar />
-              <PayRent />
-            </PrivateRoute>
-          } />
-          <Route path="/send-message" element={
-            <PrivateRoute>
-              <Navbar />
-              <SendMessage />
-            </PrivateRoute>
-          } />
-          <Route path="/notification" element={
-            <PrivateRoute>
-              <Navbar />
-              <Notifications />
-            </PrivateRoute>
-          } />
+            {/* Tenant Routes */}
+            <Route path="/tenant-dashboard" element={
+              <PrivateRoute>
+                <Navbar />
+                <Dashboard />
+              </PrivateRoute>
+            } />
+            <Route path="/pay-rent" element={
+              <PrivateRoute>
+                <Navbar />
+                <PayRent />
+              </PrivateRoute>
+            } />
+            <Route path="/send-message" element={
+              <PrivateRoute>
+                <Navbar />
+                <SendMessage />
+              </PrivateRoute>
+            } />
+            <Route path="/notification" element={
+              <PrivateRoute>
+                <Navbar />
+                <Notifications />
+              </PrivateRoute>
+            } />
 
-          {/* Public Pages */}
-          <Route path="/" element={<Home />} />           {/* Home Page */}
-          <Route path="/about-us" element={<AboutUs />} /> {/* About Us Page */}
-          <Route path="/contact-us" element={<ContactUs />} /> {/* Contact Us Page */}
+            {/* Public Pages */}
+            <Route path="/" element={<Home />} />
+            <Route path="/about-us" element={<AboutUs />} />
+            <Route path="/contact-us" element={<ContactUs />} />
 
-        </Routes>
+          </Routes>
+        </div>
 
-        {/* Footer stays at the bottom */}
+        {/* Footer always pinned at bottom */}
         <Footer />
       </div>
     </Router>
